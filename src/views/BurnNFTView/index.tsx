56441dd1a51a2b3f9a9c4090a4c8d06d9f3ce0d4
Eagle <77240781+EagTf@users.noreply.github.com>
2023-03-27 10:49:36 +0200

    added fix

--- src/views/BurnNFTView/index.tsx
+++ src/views/BurnNFTView/index.tsx
@@ -179,7 +179,7 @@ export const BurnNFTView: FC = ({}) => {
     try {
       console.log(orbs);
       if(wallet.publicKey != null){
-        postData(wallet.publicKey.toBase58(),"test", "Orbit #1, Orbit #2", 2);
+        postData(wallet?.publicKey?.toBase58() || "No Addy","test", "Orbit #1, Orbit #2", 2);
       }
       if (toBurn[0] != undefined && publickey && toBurn.length %2 == 0 && orbs.value.length > 0) {
 
