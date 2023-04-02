9587d1bc2d976067846475145d9881e6341d810b
Eagle <77240781+EagTf@users.noreply.github.com>
2023-04-01 19:17:40 -0700

    test rpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,10 @@ import "../styles/App.css";
 // const endpoint = "https://explorer-api.devnet.solana.com";
 // const endpoint = "http://127.0.0.1:8899";
 // const endpoint = "https://ssc-dao.genesysgo.net";
-// const endpoint = "https://solana-api.projectserum.com";
+ const endpoint = "https://solana-api.projectserum.com";
 //  const endpoint = "https://api.mainnet-beta.solana.com";
 // const endpoint = "https://rpc.helius.xyz/?api-key=cc778adb-f9ab-45da-ba44-b4096f663c16";
- const endpoint = "https://solana-mainnet.g.alchemy.com/v2/sYgwLczvejgglh0khGG1CVrmckCYrtIh"
+ //const endpoint = "https://solana-mainnet.g.alchemy.com/v2/sYgwLczvejgglh0khGG1CVrmckCYrtIh"
 // const endpoint = "https://try-rpc.mainnet.solana.blockdaemon.tech";
 
 const WalletProvider = dynamic(
