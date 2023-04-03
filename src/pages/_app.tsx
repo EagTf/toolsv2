ea12614171397524029d12716a8f5af95915bf12
Eagle <77240781+EagTf@users.noreply.github.com>
2023-04-03 13:43:35 -0700

    helius rpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,8 +16,9 @@ import "../styles/App.css";
 //const endpoint = "https://rpc.helius.xyz/?api-key=cc778adb-f9ab-45da-ba44-b4096f663c16";
  //const endpoint = "https://solana-mainnet.g.alchemy.com/v2/sYgwLczvejgglh0khGG1CVrmckCYrtIh"
 // const endpoint = "https://try-rpc.mainnet.solana.blockdaemon.tech";
-const endpoint = "https://little-delicate-hill.solana-mainnet.discover.quiknode.pro/65363c55d9e7e8a4bcc1a62ec422400ea0aa7b20/";
+//const endpoint = "https://little-delicate-hill.solana-mainnet.discover.quiknode.pro/65363c55d9e7e8a4bcc1a62ec422400ea0aa7b20/";
 //const endpoint = "https://rpc.ankr.com/solana/69888ad83a2afe04825141afaa54e992ba0016b33aed81f0b7202b072ed97c86";
+const endpoint = "https://rpc.helius.xyz/?api-key=071df3fc-1b02-413e-8879-fa0b6019ddf6"
 
 const WalletProvider = dynamic(
   () => import("../contexts/ClientWalletProvider"),
